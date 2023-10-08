05fd4e5eb3a9e11a6e80c8f372a948962c9bb669
Kaymartins <kayanmartins1@gmail.com>
2023-10-08 19:48:10 -0300

    une hotel e nó em uma classe só com um identificador isHotel

--- headers/Node.h
+++ headers/Node.h
@@ -3,9 +3,24 @@
 
 #include "vertex.h"
 
-//herança da classe Vertex
-class Node : public Vertex{
-
-}
+//herança da classe Vertex, é possível acessar os atributos e métodos da classe Vertex a partir da classe Node
+class Node{
+    private:
+        int id;
+        int score;
+        int x;//coordenada x
+        int y;//coordenada y
+        bool visited;
+        bool hotel;
+    public:
+        Node(int id, int score, int x, int y, bool isHotel);//construtor
+        int getId();
+        int getScore();
+        int getX();
+        int getY();
+        bool isHotel();
+        void markVisited();
+        bool isVisited();
+};
 
 #endif // NODE_H
--- src/node.cpp
+++ src/node.cpp
@@ -1,5 +1,39 @@
 #include "../headers/node.h"
 
-Node::Node(int id, int score) : Vertex(id) {
+Node::Node(int id, int score, int x, int y, bool isHotel){
+    this->id = id;
     this->score = score;
+    this->x = x;
+    this->y = y;
+    this->visited = false;
+    this->hotel = isHotel;
 }
+
+int Node::getId(){
+    return this->id;
+}
+
+int Node::getScore(){
+    return this->score;
+}
+
+int Node::getX(){
+    return this->x;
+}
+
+int Node::getY(){
+    return this->y;
+}
+
+void Node::markVisited(){
+    this->visited = true;
+}
+
+bool Node::isVisited(){
+    return this->visited;
+}
+
+bool Node::isHotel(){
+    return this->hotel;
+}
+
