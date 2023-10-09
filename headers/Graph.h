e1ae5c5473db52919086a355dfd5d71cc28f04b1
Kaymartins <kayanmartins1@gmail.com>
2023-10-08 23:15:10 -0300

    leitura e criação do grafo(criação de aresta com defeito)

--- headers/Graph.h
+++ headers/Graph.h
@@ -4,24 +4,26 @@
 #include <vector>
 #include "Node.h"
 #include "Edge.h"
-#include <unordered_set>
+#include <set>
 
 class Graph {
     private:
         std::vector<Node> nodes;//list of vertices
         std::vector<Edge> edges;//list of edges
-        double Td; //trip lenght for each trip d
-        std::unordered_set<std::pair<int, int>> addedEdges; // Track added edges
-
+        std::vector<double> Td; //trip lenght for each trip d
+        int numberOfTrips;//number of trips
+        std::set<std::pair<int, int>> addedEdges; // set to keep track of added edges
 
     public:
-        void addNode(int id, int score, int x, int y, bool isHotel, double Td);//add a node to the graph
+        void addNode(int id, int score, int x, int y, bool isHotel);//add a node to the graph
         void addEdge(Node* source, Node* destination);//add a edge to the graph
         double getDistance(Node* source, Node* destination);//get the distance between two nodes
-        double getTd();//get the trip distance
-        void setTd(double Td);//set the trip distance
+        std::vector<double> getTd();//get the trip lenght for each trip d
+        void setTd(std::vector<double> Td);//set the trip distance
+        void setNumberOfTrips(int numberOfTrips);//set the number of trips
         std::vector<Node> getNodes();//get the list of nodes
         std::vector<Edge> getEdges();//get the list of edges
+        
 };
 // Rest of the code goes here
 
